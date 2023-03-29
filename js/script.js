d7de427b083139193b962175f139b3aa5217cad8
giovannilampis <giolampis@hotmail.com>
2023-03-29 14:18:46 +0200

    add comments to script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
 "use strict"
 
+// ad an array that contains things to buy
+
 let shoppingList = ["eggs", "tuna", "olive oil", 
                     "hazelnuts", "almonds", "nuts", 
                     "peanut butter", "salmon", "carrots", 
                     "coffee", "honey", "cucumbers", "soap", 
                     "shaving cream"];
 
-let index = 0;
+// initialize an index variable with the value 0
 
-while (index < shoppingList.length) {
+let i = 0;
+
+// The while loop runs as long as index ...
+// ... is less than the length of the array
+
+while (i < shoppingList.length) {
+
+    // At each iteration, the element of the array ...
+    // ... corresponding to the index "i" is printed to the console, ...
     
-    console.log(shoppingList[index]);
+    console.log(shoppingList[i]);
 
-    index++;
+    // ... and the variable i is incremented by 1
+    
+    i++;
+
 }
