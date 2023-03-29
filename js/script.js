7661febe4002d6e921614db60cb65a45f3206f4c
giovannilampis <giolampis@hotmail.com>
2023-03-29 14:04:15 +0200

    add basic required content to project

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 "use strict"
 
+let shoppingList = ["eggs", "tuna", "olive oil", 
+                    "hazelnuts", "almonds", "nuts", 
+                    "peanut butter", "salmon", "carrots", 
+                    "coffee", "honey", "cucumbers", "soap", 
+                    "shaving cream"];
+
+let index = 0;
+
+while (index < shoppingList.length) {
+    
+    console.log(shoppingList[index]);
+
+    index++;
+}
