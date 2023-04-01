0dafae67006ab803b0d824caf29dea51fadd0ec2
giovannilampis <giolampis@hotmail.com>
2023-04-01 14:43:46 +0200

    make items appear in html

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 "use strict"
 
-// ad an array that contains things to buy
+// get #lista-spesa element in html
 
-let shoppingList = ["eggs", "tuna", "olive oil", 
+let shoppingList = document.getElementById("shopping-list");
+
+// create an array that contains things to buy
+
+let items = ["eggs", "tuna", "olive oil", 
                     "hazelnuts", "almonds", "nuts", 
                     "peanut butter", "salmon", "carrots", 
                     "coffee", "honey", "cucumbers", "soap", 
@@ -15,7 +19,16 @@ let i = 0;
 // The while loop runs as long as index ...
 // ... is less than the length of the array
 
-while (i < shoppingList.length) {
+while (i < items.length) {
+
+    const item = items[i];
+
+    const itemInList = document.createElement("li");
+
+    itemInList.innerHTML = item;
+
+    shoppingList.append(itemInList)
+
 
     // At each iteration, the element of the array ...
     // ... corresponding to the index "i" is printed to the console, ...
